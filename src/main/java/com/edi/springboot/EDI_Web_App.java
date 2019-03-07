aeaf78ed236d7db05bfed34cb580344e61bbed35
Manoranjan Das <ladu190788@gmail.com>
2019-03-07 08:17:15 +0530

    adding the react feature

---
+++ src/main/java/com/edi/springboot/EDI_Web_App.java
@@ -0,0 +1,11 @@
+package com.edi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDI_Web_App {
+   public static void main(String[] args) {
+      SpringApplication.run(EDI_Web_App.class, args);
+   }
+}
---
+++ src/main/java/com/edi/springboot/controller/EDIController.java
@@ -0,0 +1,27 @@
+package com.edi.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EDIController {
+
+  /* @RequestMapping("/")
+   public String index() {
+      return "index";
+   }
+
+   @PostMapping("/ediHome")
+   public String sayHello(@RequestParam("name") String name, Model model) {
+      model.addAttribute("name", name);
+      return "ediHome";
+   }*/
+	
+	  @RequestMapping(value="/Echo")
+	   public String echo(@RequestParam(value="request", defaultValue="Hello!") String request) {
+	      return request;
+	   }
+}
